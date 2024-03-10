22c27e2b88700468b9e0c73980e8cbc24b873126
GuiMD8 <120890296+GuiMD8@users.noreply.github.com>
2024-03-10 10:11:52 -0300

    Delete the file at 'js/gapp.js'

--- js/gapp.js
+++
@@ -1,9 +0,0 @@
-if($(".nav-current-zone").data("nav-type") === "list"){
-    if(key === "down"){
-       var currentItem = $(currentZone).find("nav-current-item");
-       var nextItem =  $(currentItem).next();
-       $(currentItem).removeClass("nav-current-item");
-       $(nextItem).addClass("nav-current-item");
-    } else if(key === "up"){
-    ...
-}
